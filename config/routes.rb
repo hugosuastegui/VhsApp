e75b359f78528861053460ac1e80f5f07e6cf710
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2019-01-19 13:53:12 -0600

    Configured custom routes for projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :projects
-  get 'pages/inicio'
-  get 'pages/nosotros'
-  get 'pages/proyectos'
-  get 'pages/contacto'
-  get 'pages/intranet'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'nosotros', to: 'pages#nosotros'
+  get 'proyectos', to: 'pages#proyectos'
+  get 'contacto', to: 'pages#contacto'
+  get 'intranet', to: 'pages#intranet'
+  
+  root to: 'pages#inicio'
+
 end
