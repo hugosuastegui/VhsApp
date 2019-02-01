dddc870e123e89f9e04204cfa0e13b78a4d4ded4
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2019-01-31 21:24:21 -0600

    Implemented Nested Attributes for Projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   enum status: { open: 0, closed: 1 }
   validates_presence_of :title, :account, :category, :client, :location, :volume
   has_many :concepts
+
+  accepts_nested_attributes_for :concepts, reject_if: lambda { |attrs| attrs['subject'].blank? }
 end
