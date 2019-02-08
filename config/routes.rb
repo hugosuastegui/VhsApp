186eba232822f7fb7653aec188e6e8d1cfb5e3b4
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2019-02-07 21:27:14 -0600

    Impleneted Devise Users migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :projects do
     member do
       get :toggle_status
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_024239) do
+ActiveRecord::Schema.define(version: 2019_02_08_031704) do
 
   create_table "concepts", force: :cascade do |t|
     t.string "subject"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2019_01_30_024239) do
     t.string "subcategory"
     t.text "description"
     t.float "amount"
-    t.string "invoice"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "invoice"
     t.integer "project_id"
     t.index ["project_id"], name: "index_concepts_on_project_id"
   end
@@ -36,6 +36,18 @@ ActiveRecord::Schema.define(version: 2019_01_30_024239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
