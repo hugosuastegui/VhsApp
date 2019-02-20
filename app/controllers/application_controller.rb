7c3ef332b901abf9505ef51bcd36826c5333972a
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2019-02-19 22:51:12 -0600

    Added guest user for nil situations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SetSource
+
+  def current_user
+    super ||  OpenStruct.new(email: "Guest")
+  end
 end
---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,10 @@
+module CurrentUserConcern
+  extend ActiveSupport::Concern
+
+  def current_user
+    super || guest_user
+  end
+  def guest_user
+    OpenStruct.new(email: "Guest")
+  end
+end
