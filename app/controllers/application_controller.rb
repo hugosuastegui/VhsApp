cc24414ab2e091544f6b1676bc31e5049dd8750c
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2019-02-20 22:20:48 -0600

    Got to commit current user concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SetSource
-
-  def current_user
-    super ||  OpenStruct.new(email: "Guest")
-  end
+  include CurrentUserConcern
 end
--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -4,7 +4,9 @@ module CurrentUserConcern
   def current_user
     super || guest_user
   end
+
   def guest_user
     OpenStruct.new(email: "Guest")
   end
+  
 end
