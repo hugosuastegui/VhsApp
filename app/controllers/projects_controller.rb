30c0a6aeeaff7a5a21e01a9cb1955313c2f4332c
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2019-02-25 20:35:54 -0600

    Added partials to forms for projects, concepts still to figure out

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy, :toggle_status]
+  layout "projects"
   
   def index
     @projects = Project.all
