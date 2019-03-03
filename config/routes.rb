00695fc7541779744c557b241164d38178b09894
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2019-03-03 13:36:01 -0600

    Added a helper to set source for every page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { registrations: "registrations" }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }, :controllers => { registrations: "registrations" }
 
   resources :projects do
     member do
